1a6e14d1a6a9d7bb9d863230764d3db94203d867
unknown <gilpa439@gmail.com>
2022-04-06 14:42:54 +0300

    CheckLegalMoves method was added

--- src/Gil.java
+++ src/Gil.java
@@ -37,10 +37,77 @@ public class Gil {
 			}
 		}
 		
-		public static void main(String[] args)
-		{
-			update_board0();
-			print_board();
+		
+		public boolean checkLegalMove (int r, int c, char direction) {
+			final int DISQ_VALUE = 0; //This value reflects the  creation of at least one trio
+
+
+			boolean validity;
+			boolean createTrio = countTriosInLocation(r, c) > DISQ_VALUE;
+			// If the indexes are out of bound or there are no trios
+			switch (direction) {
+				
+			
+			case 'u':
+				//First check if move is valid
+				validity = isLegalLoc(r, c + 1);
+				//Second check if a trio was created
+				createTrio = createTrio || countTriosInLocation(r,c + 1) > DISQ_VALUE;
+				break;
+			
+			
+			case 'd' :
+				//First check if move is valid
+				validity = isLegalLoc(r, c + 1);
+				//Second check if a trio was created
+				createTrio = createTrio || countTriosInLocation(r,c - 1) > DISQ_VALUE;
+				break;
+	
+			case 'r' :
+				//First check if move is valid
+				validity = isLegalLoc(r, c + 1);
+				//Second check if a trio was created
+				createTrio = createTrio || countTriosInLocation(r + 1,c) > DISQ_VALUE;
+				break;
+	
+			case 'l':
+				//First check if move is valid
+				validity = isLegalLoc(r, c + 1);
+				//Second check if a trio was created
+				createTrio = createTrio || countTriosInLocation(r - 1,c) > DISQ_VALUE;
+				break;
+			
+			default:
+				validity = false;
+				System.out.println("Invalid direction");
+				break;
+			}
+			
+			
+			if (!validity) {
+				System.out.println("The move is out of bound");
+				return false;
+			}
+			
+			if (!createTrio ) {
+				System.out.println("No trio was created, try again");
+			}
+			
+			
+			//If both conditions fulfilled the move is valid
+			return true;
 		}
-	}
+		
+		
+		private int countTriosInLocation(int r, int c) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public boolean isLegalLoc(int r, int c) {
+			return (r>0&&r<BOARD_SIZE&&c>0&&c<BOARD_SIZE) ;
+		}
+		
+		
+
 }
